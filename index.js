b5fe5dbc3acaadc3b58552e98b531d0086be686a
Nikolai Tatianenko <nikolai.tatianenko@gmail.com>
2021-10-24 01:36:17 +0000

    CLI | Added Help function

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 const program = require('commander');
-const checkLighthouse = require('./checkLighthouse');
+const checkLighthouse = require('./src/services/lighthouse');
 
 program
 .version('1.0.0')
@@ -17,4 +17,10 @@ program
   });
 });
 
+// Helper options.
+program.on('--help', () => {
+  console.log('');
+  console.log('Example call: node index.js https://www.google.com');
+  console.log('  $ custom-help --help');
+});
 program.parse(process.argv);

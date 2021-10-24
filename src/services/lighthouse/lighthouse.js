0620aaaec1b1b610b085ae8c9aecd15230231be1
Nikolai Tatianenko <nikolai.tatianenko@gmail.com>
2021-10-24 19:39:17 +0000

    Lighthouse | Refactor CheckUrls

--- src/services/lighthouse/lighthouse.js
+++ src/services/lighthouse/lighthouse.js
@@ -1,8 +1,6 @@
-//const fs = require('fs');
 const lighthouse = require('lighthouse');
 const chromeLauncher = require('chrome-launcher');
-//import lighthouse from 'lighthouse';
-//import chromeLauncher from 'chrome-launcher';
+
 /**
  *
  * @param urls
@@ -10,33 +8,37 @@ const chromeLauncher = require('chrome-launcher');
  */
 async function* checkUrl(urls) {
   for (const url of urls) {
-    const nStartTime = Date.now();
-    const chrome = await chromeLauncher.launch({chromeFlags: ['--headless']});
-    // logLevel: 'info',
-    const options = {output: 'json', port: chrome.port};
-    const runnerResult = await lighthouse(url, options);
+    try {
+      const nStartTime = Date.now();
+      const chrome = await chromeLauncher.launch({chromeFlags: ['--headless']});
+      // logLevel: 'info',
+      const options = {output: 'json', port: chrome.port};
+      const runnerResult = await lighthouse(url, options);
 
-    // `.report` is the HTML report as a string
-    //const reportHtml = runnerResult.report;
-    //fs.writeFileSync('lhreport.html', reportHtml);
+      // `.report` is the HTML report as a string
+      //const reportHtml = runnerResult.report;
+      //fs.writeFileSync('lhreport.html', reportHtml);
 
-    // `.lhr` is the Lighthouse Result as a JS object
+      // `.lhr` is the Lighthouse Result as a JS object
 
-    // Get timeout
-    const nEndTime = Date.now();
-    yield {
-      url: runnerResult.lhr.finalUrl,
-      'time': (nEndTime - nStartTime) / 1000,
-      'score': {
-        'accessibility': runnerResult.lhr.categories.accessibility.score * 100,
-        'best-practices': runnerResult.lhr.categories['best-practices'].score *
-          100,
-        'performance': runnerResult.lhr.categories.performance.score * 100,
-        'pwa': runnerResult.lhr.categories.pwa.score * 100,
-        'seo': runnerResult.lhr.categories.seo.score * 100,
-      },
-    };
-    await chrome.kill();
+      // Get timeout
+      const nEndTime = Date.now();
+      yield {
+        url: runnerResult.lhr.finalUrl,
+        'time': (nEndTime - nStartTime) / 1000,
+        'score': {
+          'accessibility': runnerResult.lhr.categories.accessibility.score * 100,
+          'best-practices': runnerResult.lhr.categories['best-practices'].score *
+            100,
+          'performance': runnerResult.lhr.categories.performance.score * 100,
+          'pwa': runnerResult.lhr.categories.pwa.score * 100,
+          'seo': runnerResult.lhr.categories.seo.score * 100,
+        },
+      };
+      await chrome.kill();
+    } catch (error) {
+      console.error(`Error testing URL '${url}': ${error}`);
+    }
   }
 }
 
